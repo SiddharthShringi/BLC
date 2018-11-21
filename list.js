8ac4598a0f839f2c4f797da8ba7c13da858b06b3
BlackSamsung <siddharthshringi@gmail.com>
2018-11-21 16:43:13 +0530

    make indentation of tab size 2

--- list.js
+++ list.js
@@ -1,11 +1,11 @@
 function List(id, name, boardName) {
-    this.id = id;
-    this.name = name;
-    this.boardName = boardName;
-    this.cards = [];
+	this.id = id;
+	this.name = name;
+	this.boardName = boardName;
+	this.cards = [];
 }
-List.prototype.addCard = function(name) {
-    let id = this.cards.length;
-    let newCard = new Card(id, name, this.name);
-    this.cards.push(newCard);
+List.prototype.addCard = function (name) {
+	let id = this.cards.length;
+	let newCard = new Card(id, name, this.name);
+	this.cards.push(newCard);
 }
