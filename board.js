0c302ace85ee1290581f1e2154d3cec89c95a713
BlackSamsung <siddharthshringi@gmail.com>
2018-11-21 16:36:30 +0530

    make constructor function board to create board and add addList function to its prototype

---
+++ board.js
@@ -0,0 +1,11 @@
+function Board(id, name) {
+    this.id = id;
+    this.name = name;
+    this.list = [];
+}
+
+Board.prototype.addList = function(name) {
+    var id = this.list.length
+    var newList = new List(id, name, this.name);
+    this.list.push(newList);
+}
