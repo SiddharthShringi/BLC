f88c6ec4911c4b320c6c4f34bd7d2df2d94bd7fc
BlackSamsung <siddharthshringi@gmail.com>
2018-11-23 16:20:25 +0530

    make strucure of new board

--- board.js
+++ board.js
@@ -1,34 +1,25 @@
-let listElement = document.querySelector('.list');
+class Board {
+	constructor(title) {
+		this.title = title;
+		this.lists = [];
 
-function Board(id, name) {
-	this.id = id;
-	this.name = name;
-	this.list = [];
+		this.node = createNode('div');
+		this.node.classList.add('board');
+		this.titleNode = createNode('h1');
+
+		this.listNodes = createNode('div');
+		this.listNodes.classList.add('lists');
+	}
+	render() {
+		this.titleNode.textContent = this.title;
+		this.node.appendChild(this.titleNode);
+	}
 }
 
-Board.prototype.addList = function () {
-	let name = listText.value;
-	var id = this.list.length
-	var newList = new List(id, name, this.name);
-	this.list.splice(id-1, 0, newList);
-	this.render();
-	this.buttonEvent();
-}
+document.body.onload = function() {
+	var newBoard = new Board('Task Manager');
+	newBoard.render();
+	let headElm = document.querySelector('#main');
+	headElm.appendChild(newBoard.node);
 
-Board.prototype.render = function() {
-	let listValue = (this.list.map(value => value.name)).join(' ');
-	listElement.innerHTML = listValue;
 }
-
-Board.prototype.displayBoard = function () {
-	let startList = `<input type='text' id='listText' placeholder='Add a list'><button id='listButton'>Add</button>`
-	this.list.push(new List(0, startList, this.name));
-	this.render();
-	this.buttonEvent();
-}
-Board.prototype.buttonEvent = function() {
-	let button = document.querySelector('#listButton');
-	button.addEventListener('click', this.addList.bind(this));
-}
-
-let newBoard = new Board(0, 'newBoard');
